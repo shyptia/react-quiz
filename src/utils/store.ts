d9e4999242364f6a0276bceb0ebee6fb44809797
shyptia <shyptianastya27@gmail.com>
2024-02-16 18:21:33 +0200

    update store

--- src/utils/store.ts
+++ src/utils/store.ts
@@ -3,17 +3,21 @@ import { persist } from 'zustand/middleware';
 
 type QuizState = {
   language: string;
+  gender: string;
   setLanguage: (role: string) => void;
+  setGender: (gender: string) => void;
 };
 
-export const useLanguageStore = create<QuizState>()(
+export const useQuizStore = create<QuizState>()(
   persist(
     set => ({
       language: '',
+      gender: '',
       setLanguage: (language: string) => set({ language }),
+      setGender: (gender: string) => set({gender})
     }),
     {
-      name: 'language',
+      name: 'quizStore',
     },
   ),
 );
